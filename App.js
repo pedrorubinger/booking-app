9c81c8edbce01b301ae24cca6b5c40318c55244c
Pedro Rubinger <pedro.rubinger@gmail.com>
2021-07-10 18:23:17 -0300

    feat: adds firebase package

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 import Routes from './src/routes';
 
+const firebaseConfig = {
+};
+
 export default function App() {
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  const db = firebase.firestore();
+
+  db.collection("user").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+  });
+
   return <Routes />;
 }
